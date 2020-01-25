97b779dd19bf3aa642a17a94e9845995ce11557c
Aruna Kolli <Kolli_aruna2006@yahoo.co.in>
2020-01-25 10:40:19 -0500

    initial commit

--- models/user.js
+++ models/user.js
@@ -2,11 +2,21 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: { type: String, required: true },
-  username: { type: String, required: true, unique: true },
-  password: {type: String, required: true},
+  name: {
+    type: String
+    // required: true
+  },
+  username: {
+    type: String,
+    // required: true,
+    unique: true
+  },
+  password: {
+    type: String
+    // required: true
+  },
   date: { type: Date, default: Date.now }
-}, {timestamps: true});
+}); //, {timestamps: true});
 
 const User = mongoose.model("User", userSchema);
 
--- server.js
+++ server.js
@@ -1,45 +1,58 @@
-const express = require("express")
-const app = express()
-const bcrypt = require("bcryptjs")
+const express = require("express");
+const app = express();
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
+app.use(express.json());
+var db = require("./models");
+mongoose.connect("mongodb://localhost/populatedb", { useNewUrlParser: true });
+//const users = [];
+app.use(function(req, res, next) {
+  console.log(req.method, req.url);
+  // console.log(users);
+  next();
+});
 
-app.use(express.json())
+app.get("/users", (req, res) => {
+  console.log("our .rest works!");
+  res.json(users);
+});
 
-const users = []
+app.post("/users", async (req, res) => {
+  try {
+    const salt = bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    console.log(salt);
+    console.log(hashedPassword);
+    const user = {
+      name: req.body.name,
+      password: hashedPassword,
+      username: req.body.username
+    };
+    console.log(user);
+    db.User.create(user).then(success => console.log(success)); //users.push(user);
+    res.status(201).send();
+  } catch {
+    res.status(500).send();
+  }
+});
+app.post("/users/login", async (req, res) => {
+  db.User.find({ username: req.body.username }).then(async user => {
+    console.log(user);
 
-app.get("/users", (req,res)=>{
-    res.json(users)
-})
-
-app.post("/users", async (req,res)=>{
-    try{
-        const salt = bcrypt.genSalt()
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        console.log(salt)
-        console.log(hashedPassword)
-        const user = {name: req.body.name, password: hashedPassword}
-        users.push(user)
-        res.status(201).send()
-    } catch{
-        res.status(500).send()
-    }
-
-app.post("/users/login", async (req, res)=>{
-    const user = users.find(user=> user.name=req.body.name)
-    if (user == null){
-        return res.status(400).send("Cannot find User")
-    }
-    try{
-        if(await bcrypt.compare(req.body.password, user.password)){
-           res.send("Success") 
-        }else{
-            res.send("Access Denied")
+    if (user == null) {
+      return res.status(400).send("Cannot find User");
+    } else {
+      try {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+          res.send("Success");
+        } else {
+          res.send("Access Denied");
         }
-    }catch{
-        res.status(500).send()
+      } catch {
+        res.status(500).send("why won't it work");
+      }
     }
-})
+  });
+});
 
-
-
-})
-app.listen(3000)
+app.listen(3000);
